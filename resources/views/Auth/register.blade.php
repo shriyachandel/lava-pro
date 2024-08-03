71f7c7e097d0fc8e6280aec027ffbd5037d5d735
shriya <shriyachandel1998@gmail.com>
2024-08-03 22:51:39 +0530

    register create

--- resources/views/Auth/register.blade.php
+++ resources/views/Auth/register.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.app')
 @section('register-content')
         <form class="form" method="post" action="">
-            <h2>Login</h2>
+            <h2>Register</h2>
             <div class="input">
                 <div class="inputBox">
                     <label for="uname">Username</label>
@@ -12,14 +12,18 @@
                   <input type="email" id="uemail" name="email" placeholder="Email">
               </div>
                 <div class="inputBox">
-                    <label for="">Password</label>
-                    <input type="password">
+                    <label for="upass">Password</label>
+                    <input type="password" id="upass" name="password" placeholder="Password">
                 </div>
                 <div class="inputBox">
-                    <input type="submit" name="" value="Sign In"> 
+                  <label for="cpass">Confirm Password</label>
+                  <input type="password" id="cpass" name="password_confirmation" placeholder="Confirm Password">
+              </div>
+                <div class="inputBox">
+                    <input type="submit" name="" value="Sign Up"> 
                 </div>
             </div>
-            <p class="forgot">Forgot Password? <a href="#">Click Here</a></p>
+            <p class="forgot">Already have an account? <a href="{{route('login')}}">Login</a></p>
             <div class="social">
                 <button><i class="fa fa-facebook" aria-hidden="true"></i><p>Signin with Facebook</p></button>
                 <button><i class="fa fa-twitter" aria-hidden="true"></i><p>Signin with Twitter</p></button>
