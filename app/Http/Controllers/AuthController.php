336207d9e5ed19c28185849d333ac1067e5f0227
shriya <shriyachandel1998@gmail.com>
2024-08-04 17:22:06 +0530

    register css

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator; // import validator to use validator
+use App\Models\User; // Import the User model
 
 class AuthController extends Controller
 {
