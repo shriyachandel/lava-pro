739e9a996244bcd5b2f9a2ed364d91d5e49f0691
shriya <shriyachandel1998@gmail.com>
2024-08-04 15:56:41 +0530

    register validation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,4 +29,21 @@ class AuthController extends Controller
    function register(){
     return view('auth.register');
    }
+   function registerprocess(){
+    $validator = validator::make($request->all(),[
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|string|min:8||regex:/[A-Z]/|regex:/[a-z]|regex:/[0-9]/|regex:/[@$!%*?&#]/|confirmed',
+        'age'=>'required|numeric|max:3',
+        'gander'=> 'required|in:Male,Female,Other',
+        'city' => 'required|string',
+    ]);
+    if($validator->passes()){
+
+    }else{
+        return redirect()->route('register.view')
+        ->withInput() // this is used to get old value from input fields
+        ->withErrors($validator);
+    }
+   }
 }
